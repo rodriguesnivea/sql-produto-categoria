72233d848c4ba23f098e25a4b175b36739a5282d
rodriguesnivea <rodriguesnivea779@gmail.com>
2022-09-05 22:52:10 -0300

    insercao de dados na tabela produto_categoria

---
+++ Scripts/produto_categoria.sql
@@ -0,0 +1,33 @@
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(5, 2);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(3, 2);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(2, 2);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(1, 2);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(5, 2);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(4, 2);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(8, 3);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(9, 3);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(10, 6);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(6, 1);
+INSERT INTO public.produto_categoria
+(produto_id, categoria_id)
+VALUES(7, 1);
